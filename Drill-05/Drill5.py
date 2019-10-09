f6eb2c074b358444feaab65e8d54308beedf1d43
hanGB(C221) <sy0871@naver.com>
2019-10-09 16:33:42 +0900

    Drill5 show hand_arrow

--- Drill-05/Drill5.py
+++ Drill-05/Drill5.py
@@ -10,19 +10,27 @@ character = load_image("animation_sheet.png")
 
 def input_mouse():
     global game
+    global x, y
 
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             game = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT- 1 - event.y
 
 game = True
+
+x, y = KPU_WIDTH / 2, KPU_HEIGHT / 2
+
+hide_cursor()
 
 while game:
     clear_canvas()
 
     kpu.draw(KPU_WIDTH / 2, KPU_HEIGHT / 2)
+    hand_arrow.draw(x, y)
     input_mouse()
 
     update_canvas()
