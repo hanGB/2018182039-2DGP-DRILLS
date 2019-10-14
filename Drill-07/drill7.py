1ee7c4f64c12c862054f0f9bfb734b1fdcf1008f
hanGB(C106) <sy0871@naver.com>
2019-10-14 12:02:04 +0900

    drill7 공 그리기
    2018182039 한규범 drill7 커밋횟수 2

--- Drill-07/drill7.py
+++ Drill-07/drill7.py
@@ -20,20 +20,35 @@ class Boy:
 
     def update(self):
         self.frame = (self.frame + 1) % 8
-        self.x += 5
+        self.x += 0.5
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.size = random.randint(0, 1)
+        self.speed = random.randint(1, 100)
+        self.falling = True
+        if self.size == 0:
+            self.image = load_image("ball21x21.png")
+        elif self.size == 1:
+            self.image = load_image("ball41x41.png")
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
 def handle_events():
-    global running
+    global game
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            game = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+            game = False
 
 # initialization code
 
@@ -43,12 +58,13 @@ open_canvas()
 grass = Grass()
 # boy = Boy()
 team = [Boy() for i in range(11)]
-running = True
+falling_balls = [Ball() for i in range(20)]
+game = True
 
 # game main loop code
 
 
-while running:
+while game:
     handle_events()
 
     for boy in team:
@@ -59,9 +75,11 @@ while running:
 
     for boy in team:
         boy.draw()
-    update_canvas()
 
-    delay(0.05)
+    for ball in falling_balls:
+        ball.draw()
+
+    update_canvas()
 
 # finalization code
 
